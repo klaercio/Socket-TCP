3d5204eba48b24b98eb04ef9b99d84c19afa6871
Klaercio <klaerciosilva@gmail.com>
2022-12-07 09:59:54 -0300

    ➕Mudança no retorno do servidor

--- TCPClient.py
+++ TCPClient.py
@@ -1,10 +1,11 @@
-from socket import *
+from socket import *;
+
 serverName = '127.0.0.1'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
-sentence = input('Input lowercase sentence:')
+sentence = input('Escreva uma expressão matemática:')
 clientSocket.send(sentence.encode())
 modifiedSentence = clientSocket.recv(1024)
-print ("From Server:",  modifiedSentence)
+print ("Resposta do servidor:",  modifiedSentence.decode())
 clientSocket.close()
--- TCPServer.py
+++ TCPServer.py
@@ -3,11 +3,11 @@ serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
-print ("The server is ready to receive")
+print ("O servidor está pronto para receber")
 
 while 1:
 	connectionSocket, addr = serverSocket.accept()
 	sentence = connectionSocket.recv(1024)
-	capitalizedSentence = sentence.upper()
-	connectionSocket.send(capitalizedSentence)
+	soma = str(eval(sentence.decode())).encode()
+	connectionSocket.send(soma)
 	connectionSocket.close()
